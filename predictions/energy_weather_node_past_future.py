2cdb115d018e017d8eac2ed4459d9e2d94d92a2b
mareitzef <matze.reif@posteo.de>
2025-11-29 14:06:13 +0100

    tututuuu

--- predictions/energy_weather_node_past_future.py
+++ predictions/energy_weather_node_past_future.py
@@ -209,6 +209,7 @@ def main():
     today = datetime.today()
 
     # start date is one week before today
+    location = "Default - Bad Krotzingen"
     nr_days = 7
     first_date = datetime.today() - timedelta(days=nr_days)
     # OpenWeatherMap API key
